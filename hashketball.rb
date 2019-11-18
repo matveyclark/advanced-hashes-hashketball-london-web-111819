329bf0b72eda0fb11bbc128a07e7b04bb03a6288
Matthew <matthew.97.clark@gmail.com>
2019-11-18 13:12:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def game_hash
     },
     :away => {
       :team_name => 'Charlotte Hornets',
-      :colors => ['Turquoise', 'White'],
+      :colors => ['Turquoise', 'Purple'],
       :players => [
           {
             :player_name => "Alan Anderson",
