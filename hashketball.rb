bded1d89ec54ab6abd4b6c57cee9b8bb81934e91
Matthew <matthew.97.clark@gmail.com>
2019-11-18 13:10:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -47,6 +47,17 @@ def game_hash
             :steals => 3,
             :blocks => 1,
             :slam_dunks => 15
+          },
+          {
+            :player_name => "Mason Plumlee",
+            :number => 1,
+            :shoe => 17,
+            :points => 17,
+            :rebounds => 19,
+            :assists => 10,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 15
           }
         ]
     },
