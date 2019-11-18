5f97b692c04450975532b195096a251f695c011e
Matthew <matthew.97.clark@gmail.com>
2019-11-18 13:10:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,7 +55,7 @@ def game_hash
             :points => 19,
             :rebounds => 2,
             :assists => 2,
-            :steals => 3,
+            :steals => 4,
             :blocks => 1,
             :slam_dunks => 15
           }
